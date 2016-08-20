7b7e9d3e0bb278f29fddea3f4b40a5c66956efa0
PigeonJeorge <alex.steven.elias@gmail.com>
2016-08-19 17:32:43 -0700

    fixed issue from chaning the index

--- test/read-write-line.js
+++ test/read-write-line.js
@@ -1,18 +1,18 @@
 'use strict';
 
 var Fsep = require('../index.js');
+var When = require('when');
 
 var options = {
 	read: {
 		path: './rw/one.txt'
 	},
 	write: {
-		path: './rw/two.txt',
-		flags: 'a'
+		path: './rw/two.txt'
 
 	},
 	line: function (line) {
-		return line.toUpperCase();
+		return When.promise.resolve().then(function () { line.toUpperCase(); });
 	}
 };
 
