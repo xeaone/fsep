390c1cd793ff7dbe6c0d58d7133c86da8b28d586
PigeonJeorge <alex.steven.elias@gmail.com>
2016-08-23 09:19:36 -0700

    added ensure dir

--- test/ensure-dir.js
+++ test/ensure-dir.js
@@ -1,9 +1,9 @@
+
 var Fsep = require('../index');
 var Path = require('path');
 
-var path = Path.join(process.cwd(), 'rw/blue.txt');
-var data = 'hello';
+var path = Path.join(process.cwd(), 'rw/blue/blah');
 
-Fsep.outputFile(path, data, 'utf8').then(function () {
+Fsep.ensureDir(path).then(function () {
 	console.log('done');
 }).catch(function (error) { throw error; });
