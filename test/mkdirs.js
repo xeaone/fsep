2b85bea179396b37eaa2301b72978ac1b96330eb
Alexander Elias <alex.steven.elias@gmail.com>
2017-01-20 00:51:08 -0700

    fixed eexist error

--- test/mkdirs.js
+++ test/mkdirs.js
@@ -3,7 +3,19 @@ const Path = require('path');
 
 var path = Path.join(__dirname, 'rw/red/green');
 
-Fsep.mkdirs(path).then(function () {
+Promise.resolve().then(function () {
+
+	Fsep.mkdirs(path).catch(function (error) {
+		console.log(error);
+	});
+
+	Fsep.mkdirs(path).catch(function (error) {
+		console.log(error);
+	});
+
+}).then(function () {
+	return Fsep.mkdirs(path);
+}).then(function () {
 	console.log('done');
 }).catch(function (error) {
 	throw error;
