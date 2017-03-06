de96a681b489a55fd3b0552f16b37af09b40408b
Alexander Elias <alex.steven.elias@gamil.com>
2017-03-06 12:42:20 -0700

    fixed issues with async Fs.stats

--- test/mkdirs.js
+++ test/mkdirs.js
@@ -1,22 +1,33 @@
 const Fsep = require('../index.js');
 const Path = require('path');
 
-var path = Path.join(__dirname, 'rw/red/green');
+var relativePath = 'rw/red/green';
+var absolutePath = Path.join(__dirname, 'rw/red/green');
 
 Promise.resolve().then(function () {
 
-	Fsep.mkdirs(path).catch(function (error) {
-		console.log(error);
+	Fsep.mkdirs(relativePath, 'foo/bar/').catch(function (error) {
+		console.error(error);
 	});
 
-	Fsep.mkdirs(path).catch(function (error) {
-		console.log(error);
+	Fsep.mkdirs(relativePath, __dirname).catch(function (error) {
+		console.error(error);
 	});
 
-}).then(function () {
-	return Fsep.mkdirs(path);
+	Fsep.mkdirs(absolutePath, __dirname).catch(function (error) {
+		console.error(error);
+	});
+
+	Fsep.mkdirs(relativePath).catch(function (error) {
+		console.error(error);
+	});
+
+	Fsep.mkdirs(absolutePath).catch(function (error) {
+		console.error(error);
+	});
+
 }).then(function () {
 	console.log('done');
 }).catch(function (error) {
-	throw error;
+	console.error(error);
 });
--- test/output-file.js
+++ test/output-file.js
@@ -1,7 +1,7 @@
 var Fsep = require('../index');
 var Path = require('path');
 
-var path = Path.join(process.cwd(), 'rw/blue.txt');
+var path = Path.join(process.cwd(), 'rw/stuff/la/blue.js');
 var data = 'hello';
 
 Fsep.outputFile(path, data, 'utf8').then(function () {
